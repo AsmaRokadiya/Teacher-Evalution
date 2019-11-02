75db9c5ad1ddec8790c265d51d10deeb75951114
sonym3 <Bony#9400>
2019-11-02 17:17:18 -0400

    login page auth done

--- app/src/main/java/com/example/asmar/teacherassesment/MainActivity.java
+++ app/src/main/java/com/example/asmar/teacherassesment/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.asmar.teacherassesment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+        private String uname="student1";
+    private String pwd="abcd";
+    private Button btn;
+    EditText user_id,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user_id = (EditText)findViewById(R.id.userid);
+        password = (EditText) findViewById(R.id.password);
+        btn= (Button) findViewById(R.id.btnlogin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(password.getText().toString().equals(pwd)&&user_id.getText().toString().equals(uname)){
+                    Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_LONG).show();
+                    Intent i=new Intent(getApplicationContext(),Main2Activity.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
